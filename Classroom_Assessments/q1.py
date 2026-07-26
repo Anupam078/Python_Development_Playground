91bea837024c936a57b1a7f522f2707c8d76dcc8
Anupam Ojha <anupamojha078@gmail.com>
2026-07-26 15:22:52 +0530

    Initial commit

---
+++ Classroom_Assessments/q1.py
@@ -0,0 +1,19 @@
+# Different data types in Python
+
+integer = 10
+floating = 10.5
+text = "Hello"
+boolean = True
+list1 = [1, 2, 3]
+tuple1 = (4, 5, 6)
+set1 = {7, 8, 9}
+dict1 = {"name": "John", "age": 20}
+
+print(integer, type(integer))
+print(floating, type(floating))
+print(text, type(text))
+print(boolean, type(boolean))
+print(list1, type(list1))
+print(tuple1, type(tuple1))
+print(set1, type(set1))
+print(dict1, type(dict1))
---
+++ Classroom_Assessments/q2.py
@@ -0,0 +1,22 @@
+# Creating strings
+
+str1 = "Hello"
+str2 = "World"
+
+# Concatenation
+print("Concatenation:", str1 + " " + str2)
+
+# Duplication
+print("Duplication:", str1 * 3)
+
+# String Indexing
+print("First character:", str1[0])
+print("Last character:", str1[-1])
+
+# String Slicing
+print("Slice (0:4):", str1[0:4])
+print("Slice (1:5):", str1[1:5])
+
+# String Membership
+print("'H' in str1:", 'H' in str1)
+print("'z' in str1:", 'z' in str1)
