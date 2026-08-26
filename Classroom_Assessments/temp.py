db8cc3e06722f761cf3e2cda22d98cc1ceea2644
Anupam Ojha <anupamojha078@gmail.com>
2026-08-26 12:08:08 +0530

    ClassRoom notes of python

--- Classroom_Assessments/temp.py
+++ Classroom_Assessments/temp.py
@@ -1,13 +1,5 @@
-shop = {'item1': 45.50, 'item2': 35, 'item3': 41.30, 'item4': 55, 'item5': 24}
- 
-items = list(shop.items())
- 
-for i in range(len(items)):
-    for j in range(len(items) - i - 1):
-        if items[j][1] < items[j + 1][1]:
-            temp = items[j]
-            items[j] = items[j + 1]
-            items[j + 1] = temp
- 
-for i in range(3):
-    print(items[i][0], items[i][1])
+a_int = 1
+b_float = 1.0
+c_sum = a_int + b_float
+print(c_sum)
+print(type(c_sum))
