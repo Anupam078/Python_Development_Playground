30ec98583bac64200647a50926ab658df76fc2e4
Anupam Ojha <anupamojha078@gmail.com>
2026-09-09 20:36:14 +0530

    Practiced File Handeling functions

--- Classroom_Assessments/File_Handeling/Basics.py
+++ Classroom_Assessments/File_Handeling/Basics.py
@@ -3,3 +3,16 @@ with open("exam_log.txt" , "w") as log_file:
 
 with open("exam_log.txt" , "a") as log_file:
     log_file.write("User logged out.\n")
+
+with open("server_log.txt","r") as read_log:
+    log_list=read_log.readlines()
+
+Error_log=[]
+for line in log_list:
+    words=line.split()
+    for word in words:
+        if "ERROR" in line:
+            Error_log.append(line)
+
+with open("error_logs.txt","w") as write_error:
+    write_error.writelines(Error_log)
